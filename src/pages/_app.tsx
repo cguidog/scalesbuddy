79b2dfb90669b8724d64c212bd8c5bfe1d0f0589
Carlos <v-carlosgui@microsoft.com>
2023-05-18 19:03:13 -0400

    Updated script loading strategy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return <>
   <Component {...pageProps} />
   <Script src="https://www.googletagmanager.com/gtag/js?id=UA-141668348-3" />
-      <Script id="gtag" dangerouslySetInnerHTML={{
+      <Script id="gtag" strategy='lazyOnload' dangerouslySetInnerHTML={{
         __html: `window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'UA-141668348-3');`
       }} />
-      <Script id="gtm" dangerouslySetInnerHTML={{
+      <Script id="gtm" strategy='lazyOnload' dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
