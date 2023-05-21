2d27bae625c345d2e6ac73ae993a7c12a76f594a
Carlos <v-carlosgui@microsoft.com>
2023-05-21 15:20:54 -0400

    Added transparent outline to buttons to improve high-contrast accessibility

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,7 @@ const Home = () => {
               <h3 className='text-lg md:text-2xl text-center'>Note</h3>
               <div className='flex w-full justify-between'>
                 {notes.map((item) => {
-                  return <button type='button' key={notes.indexOf(item)} aria-label={`${item} note`} className={`${note === item ? 'bg-emerald-700' : 'bg-gray-500 hover:bg-gray-600'} h-fit px-4 py-2 rounded-sm shadow-equal-sm shadow-black text-slate-50 transition-all ease-in-out duration-150 active:scale-95 active:shadow-none focus-visible:outline focus-visible:outline-black focus-visible:outline-4`} onClick={() => setNote(item as 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G')} >{item}</button>
+                  return <button type='button' key={notes.indexOf(item)} aria-label={`${item} note`} className={`${note === item ? 'bg-emerald-700' : 'bg-gray-500 hover:bg-gray-600'} h-fit outline outline-transparent outline-2 px-4 py-2 rounded-sm shadow-equal-sm shadow-black text-slate-50 transition-all ease-in-out duration-150 active:scale-95 active:shadow-none focus-visible:outline focus-visible:outline-black focus-visible:outline-4`} onClick={() => setNote(item as 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G')} >{item}</button>
                 })}
               </div>
             </div>
@@ -49,19 +49,19 @@ const Home = () => {
               <div className='flex flex-1 flex-col gap-4'>
                 <h3 className='text-lg md:text-2xl text-center'>Scale</h3>
                 <div className='flex gap-8 lg:gap-4 justify-center lg:justify-between'>
-                  {scales.map(item => <button type='button' key={scales.indexOf(item)} aria-label={`${item} scale`} className={`${scale === item ? 'bg-emerald-700' : 'bg-gray-500 hover:bg-gray-600'} capitalize h-fit w-20 py-2 rounded-sm shadow-equal-sm shadow-black text-slate-50 transition-all ease-in-out duration-150 active:scale-95 active:shadow-none focus-visible:outline focus-visible:outline-black focus-visible:outline-4`} onClick={() => setScale(item as 'major' | 'minor')}>{item}</button>)}
+                  {scales.map(item => <button type='button' key={scales.indexOf(item)} aria-label={`${item} scale`} className={`${scale === item ? 'bg-emerald-700' : 'bg-gray-500 hover:bg-gray-600'} capitalize h-fit w-20 outline outline-transparent outline-2 py-2 rounded-sm shadow-equal-sm shadow-black text-slate-50 transition-all ease-in-out duration-150 active:scale-95 active:shadow-none focus-visible:outline focus-visible:outline-black focus-visible:outline-4`} onClick={() => setScale(item as 'major' | 'minor')}>{item}</button>)}
                 </div>
               </div>
               <div className='flex flex-1 flex-col gap-4'>
                 <h3 className='text-lg md:text-2xl text-center'>Position</h3>
                 <div className='flex gap-8 lg:gap-4 justify-center lg:justify-between'>
-                  {positions.map(item => <button type='button' key={positions.indexOf(item)} aria-label={`${item} position`} className={`${position == item ? 'bg-emerald-700' : 'bg-gray-500 hover:bg-gray-600'} capitalize h-fit w-20 py-2 rounded-sm shadow-equal-sm shadow-black text-slate-50 transition-all ease-in-out duration-150 active:scale-95 active:shadow-none focus-visible:outline focus-visible:outline-black focus-visible:outline-4`} onClick={() => setPosition(item as 'first' | 'second')}>{item}</button>)}
+                  {positions.map(item => <button type='button' key={positions.indexOf(item)} aria-label={`${item} position`} className={`${position == item ? 'bg-emerald-700' : 'bg-gray-500 hover:bg-gray-600'} capitalize h-fit w-20 outline outline-transparent outline-2 py-2 rounded-sm shadow-equal-sm shadow-black text-slate-50 transition-all ease-in-out duration-150 active:scale-95 active:shadow-none focus-visible:outline focus-visible:outline-black focus-visible:outline-4`} onClick={() => setPosition(item as 'first' | 'second')}>{item}</button>)}
                 </div>
               </div>
               <div className='flex flex-1 flex-col gap-4'>
                 <h3 className='text-lg md:text-2xl text-center'>Instrument</h3>
                 <div className='flex gap-8 lg:gap-4 justify-center lg:justify-between'>
-                  {instruments.map(item => <button type='button' key={instruments.indexOf(item)} aria-label={`Instrument: ${item}`} className={`${instrument === item ? 'bg-emerald-700' : 'bg-gray-500 hover:bg-gray-600'} capitalize h-fit w-20 py-2 rounded-sm shadow-equal-sm shadow-black text-slate-50 transition-all ease-in-out duration-150 active:scale-95 active:shadow-none focus-visible:outline focus-visible:outline-black focus-visible:outline-4`} onClick={() => setInstrument(item)}>{item}</button>)}
+                  {instruments.map(item => <button type='button' key={instruments.indexOf(item)} aria-label={`Instrument: ${item}`} className={`${instrument === item ? 'bg-emerald-700' : 'bg-gray-500 hover:bg-gray-600'} capitalize h-fit w-20 outline outline-transparent outline-2 py-2 rounded-sm shadow-equal-sm shadow-black text-slate-50 transition-all ease-in-out duration-150 active:scale-95 active:shadow-none focus-visible:outline focus-visible:outline-black focus-visible:outline-4`} onClick={() => setInstrument(item)}>{item}</button>)}
                 </div>
               </div>
             </div>
@@ -69,7 +69,7 @@ const Home = () => {
           { allSelected && <div className='flex flex-col gap-4 items-center'>
             <h3 className='m-auto text-center text-lg md:text-2xl'>{`${note?.toUpperCase()} ${scale}`} scale - <span className='capitalize'>{position}</span> position - <span className='capitalize'>{instrument}</span></h3>
           </div>}
-          {note && scale && instrument && position && <button id='continue_button' disabled={settings} className={`${allSelected? 'bg-red-700' : 'bg-emerald-700'} h-fit w-28 m-auto px-4 py-2 rounded-sm shadow-equal-sm shadow-black text-slate-50 transition-all ease-in-out duration-150 active:scale-95 active:shadow-none disabled:cursor-not-allowed disabled:opacity-60  disabled:scale-100 disabled:shadow-none focus-visible:outline focus-visible:outline-black focus-visible:outline-4`} onClick={()=>handleSelected()}>{allSelected ? 'Edit' : 'Continue'}</button>}
+          {note && scale && instrument && position && <button id='continue_button' disabled={settings} className={`${allSelected? 'bg-red-700' : 'bg-emerald-700'} h-fit w-28 m-auto outline outline-transparent outline-2 px-4 py-2 rounded-sm shadow-equal-sm shadow-black text-slate-50 transition-all ease-in-out duration-150 active:scale-95 active:shadow-none disabled:cursor-not-allowed disabled:opacity-60  disabled:scale-100 disabled:shadow-none focus-visible:outline focus-visible:outline-black focus-visible:outline-4`} onClick={()=>handleSelected()}>{allSelected ? 'Edit' : 'Continue'}</button>}
         </div>
         {allSelected && <Fretboard settings={settings} handleSettings={handleSettings} note={note} scale={scale} instrument={instrument} position={position} />}
       </main>
