78bc583262d62e446fc7e584ecf1e118e9c0cc60
Carlos <v-carlosgui@microsoft.com>
2023-06-28 10:38:01 -0400

    Added G4 analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,21 @@ import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-  <Component {...pageProps} />
-  <Script src="https://www.googletagmanager.com/gtag/js?id=UA-141668348-3" />
-      <Script id="gtag" strategy='lazyOnload' dangerouslySetInnerHTML={{
-        __html: `window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'UA-141668348-3');`
-      }} />
-      <Script id="gtm" strategy='lazyOnload' dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <Component {...pageProps} />
+    <Script id="g4link" src="https://www.googletagmanager.com/gtag/js?id=G-J2M3GMW2PF" />
+    <Script id="g4tag" strategy='lazyOnload' dangerouslySetInnerHTML={{
+      __html: `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-J2M3GMW2PF');`
+    }} />
+    <Script id="gtm" strategy='lazyOnload' dangerouslySetInnerHTML={{
+      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-5526R3Q');`
-      }} />
+    }} />
   </>
 }
